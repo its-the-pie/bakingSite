b4a5f5abaa1414e47d9e83abd730b78b38f922b0
its-the-pie <farleyy2007@gmail.com>
2025-07-21 16:18:59 -0400

    oopsies

---
+++ recipes/recipes.js
@@ -0,0 +1,43 @@
+let recipes = [
+    { recipe: 'Blueberry Cake', type: 'cake', v: 1, gf: -1, sf: 1, ingredients: 'ingredient 1\ningredient 2', method: 'method'},
+    { recipe: 'Brazilian Beijinho', type: 'otherDes', v: 0, gf: 1, sf: -1, ingredients: 'ingredient 1\ningredient 2', method: 'method'},
+    { recipe: 'Brazilian Brigadeiro', type: 'otherDes', v: 0, gf: 1, sf: -1, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Brookies', type: 'cookie', v: 0, gf: -1, sf: -1, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Brownies', type: 'cookie', v: 0, gf: -1, sf: -1, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Chia Seed Pudding', type: 'otherDes', v: 0, gf: 0, sf: 0, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Chocolate Chip Cookies', type: 'cookie', v: 0, gf: -1, sf: -1, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Chocolate Cake', type: 'cake', v: 0, gf: -1, sf: -1, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Coconut Strawberry Cake', type: 'cake', v: 0, gf: -1, sf: -1, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Crepes', type: 'breakfast', v: -1, gf: -1, sf: 1, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Lava Cake', type: 'cake', v: -1, gf: -1, sf: -1, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Date Lava Cake', type: 'cake', v: 0, gf: -1, sf: 0, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Maple Pecan Pie', type: 'pie', v: -1, gf: 0, sf: 0, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Vegan Pecan Pie', type: 'pie', v: 1, gf: 0, sf: 0, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'NYT Pie Crust', type: 'otherMisc', v: -1, gf: -1, sf: 0, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Vegan Pie Crust', type: 'otherMisc', v: 1, gf: -1, sf: 0, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Oat Pie Crust', type: 'otherMisc', v: 0, gf: 1, sf: 0, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Pao Frances', type: 'bread', v: 1, gf: -1, sf: 0, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Rhubarb Cake', type: 'cake', v: 0, gf: -1, sf: 0, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Tiramisu', type: 'otherDes', v: -1, gf: 0, sf: -1, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Vegan Tiramisu', type: 'otherDes', v: 1, gf: 0, sf: -1, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Tiramisu Cake', type: 'cake', v: 0, gf: -1, sf: -1, ingredients: 'ingredent 1\ningredient 2', method: 'method'},
+    { recipe: 'Vegan Date Custard', type: 'otherDes', v: 1, gf: 1, sf: 1, ingredients: 'ingredient 1\ningredient 2', method: 'method'}
+]
+
+function allRecipes() {}
+function breads() {}
+function cookies() {}
+function pies() {}
+function cakes() {}
+function breakfast() {}
+function lunch() {}
+function dinner() {}
+function appets() {}
+function snacks() {}
+function otherDes() {}
+function otherMisc() {}
+
+
+
+//recipe; category; vegan; gluten-free; refined-sugar-free; ease
+// -1 = no, 0 = can be, 1 = yes
